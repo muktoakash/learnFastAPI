8a9947a7dfababc0552b1c43778c2f80a1cfc329
Mukto Akash <73410983+muktoakash@users.noreply.github.com>
2024-06-12 12:48:49 +0000

    Create a task using BaseModel from pydantic

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List, Optional
+from uuid import UUID, uuid4
 
 app = FastAPI()
+
+class Task(BaseModel):
+    id: Optional[UUID] = None
+    title: str
+    description: Optional[str] = None
+    completed: bool = False
+
+tasks = []
+
+@app.post("/tasks/", response_model=Task)
+def create_task(task: Task):
+    task.id = uuid4()
+    tasks.append(task)
+    return task
 
 @app.get("/")
 def read():
