52a14c8c0c1f6ff6855c554551858a166b7c00f2
Mukto Akash <73410983+muktoakash@users.noreply.github.com>
2024-06-12 13:01:42 +0000

    Create Delete method

--- main.py
+++ main.py
@@ -41,6 +41,13 @@ def update_task(task_id: UUID, task_update: Task):
 
     raise HTTPException(status_code=404, detail="Task not found")
 
+@app.delete("/tasks/{task_id}", response_model=Task)
+def delete_task(task_id):
+    for idx, task in enumerate(tasks):
+        if task.id == task_id:
+            return tasks.pop(idx)
+
+    raise HTTPException(status_code=404, detail="Task not found")
 
 if __name__ == "__main__":
     import uvicorn
