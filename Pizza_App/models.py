91700829f9de5283cb39198159a9fb27da6c7332
Mukto Akash <muktoakash@gmail.com>
2024-07-05 22:11:41 -0400

    Set up models.py

---
+++ Pizza_App/models.py
@@ -0,0 +1,14 @@
+from database import Base
+from sqlalchemy import Column, Integer, Boolean, Text, String
+
+class User(Base):
+    __tablename__ = 'user' # usertable
+    id = Column(Integer, primary_key = True)
+    username = Column(String(25), unique=True)
+    email = Column(String(80), unique=True)
+    password = Column(Text, nullable=True)
+    is_staff = Column(Boolean, default=False)
+    is_active = Column(Boolean, default=False)
+
+    def __repr__(self):
+        return f"<User {self.username}>"
