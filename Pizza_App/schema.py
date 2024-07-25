1881d9657edca769a2a59cce99e18ab64a6d9b80
Mukto Akash <muktoakash@gmail.com>
2024-07-25 07:05:28 -0400

    Create a SignUpModel

--- Pizza_App/schema.py
+++ Pizza_App/schema.py
@@ -1,3 +1,24 @@
 """ schema.py """
 
 from pydantic import BaseModel
+from typing import Optional
+
+class SignUpModel(BaseModel):
+    id : Optional[int]
+    username : str
+    email : str
+    password : str
+    is_staff : Optional[bool]
+    is_active : Optional[bool]
+
+    class config:
+        orm_mode = True
+        schema_extra = {
+            'example' : {
+                'username': "johndoe",
+                'email': "johndoe@skiff.com",
+                'password': "hunter2",
+                'is_staff' : False,
+                'is_active' : True
+            }
+        }
